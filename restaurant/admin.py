96c29e351c21224d3b75004b0ada34b45a0d2a1f
=mazinAbbasher <=mazinabbayo@gmail.com>
2023-08-24 13:03:16 +0200

    setting up the models

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking,Menu
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display=['title','price','inventory']
+
+
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display=['name','no_of_guests','booking_date']
+
+admin.site.register(Menu,MenuAdmin)
+admin.site.register(Booking,BookingAdmin)
